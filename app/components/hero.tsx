b73da415e89beba716880e2e10e019dcf6397f5c
Your Name <rparker.ccs@gmail.com>
2025-03-20 08:45:28 -0700

    push changes

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -99,7 +99,7 @@ export default function Hero() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.2 }}
         >
-          Visual Artist & Creative Director
+          I LOVE YOU GABBYYYYY
         </motion.p>
       </div>
     </div>
